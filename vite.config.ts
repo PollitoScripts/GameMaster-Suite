b77cab81d156ddf19ef0f744d15d69eac222f50c
Pollito420 <69217344+PollitoScripts@users.noreply.github.com>
2026-05-18 17:38:50 +0200

    Configuration for deploy in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asegura compatibilidad absoluta con rutas relativas de GitHub Pages
+  base: '/Ruleta-Multiplayer/', // 👈 ¡Solo el nombre del repositorio entre barras diagonales!
   build: {
     outDir: 'dist',
     sourcemap: false
