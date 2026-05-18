a8cae504180a30a02ff94dfcb45ca6dc3f0dbbfb
Pollito420 <69217344+PollitoScripts@users.noreply.github.com>
2026-05-18 19:07:42 +0200

    for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ruleta-Multiplayer/', // 👈 ¡Solo el nombre del repositorio entre barras diagonales!
+  base: '/gamemaster-suite/', // 👈 ¡Solo el nombre del repositorio entre barras diagonales!
   build: {
     outDir: 'dist',
     sourcemap: false
